7e9520048ae9d1ce5b82365c23c8ee7bba496aa2
William Bergeron <2052824@cstj.qc.ca>
2020-09-16 12:42:59 -0400

    Ajoutez des fichiers projet.

---
+++ ProjetWilliamBergeron/Location.cpp
@@ -0,0 +1,51 @@
+// But : Écrire un programme qui calcule le montant d'un individu devra payer pour faire un voyage avec une voiture louée.
+//		 *Il devra prendre compte du kilométrage parcouru et de la durée du voyage.
+// Auteur : William Bergeron
+// Date : 2020-09-16
+
+#include <iostream>
+
+// Déclaration des variables
+
+
+
+
+
+
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
